8e83685ecb0877997cf35e6bd450894fa56bf2d8
jkv <jenskristianvilladsen@gmail.com>
2020-01-05 12:17:41 +0100

    Added actuator for docker readiness endpoint

--- cda-validator-schematron/src/main/java/org/artdecor/Application.java
+++ cda-validator-schematron/src/main/java/org/artdecor/Application.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
+    System.setProperty("management.endpoint.info.enabled","true");
     SpringApplication.run(Application.class, args);
   }
 }
